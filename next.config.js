1520ad7ba50ee47e8c8a084240aed01cfcdf7960
Ivan Deiko <IvanDeiko97@gmail.com>
2019-08-21 08:48:28 +0300

    'fix'

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const debug = process.env.NODE_ENV !== "production";
+module.exports = {
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      "/cart": { page: "/cart" },
+    }
+  },
+  assetPrefix: !debug ? 'https://ivandeiko97.github.io/shop/' : '',
+}
